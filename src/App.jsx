0638cf6915b3421ec79b940aff9f00352537ab55
Naphat Kaorobphatai <naphat.k64@rsu.ac.th>
2023-06-23 03:20:28 +0700

    color

--- src/App.jsx
+++ src/App.jsx
@@ -14,6 +14,18 @@ import TableContainer from "@mui/material/TableContainer";
 import TableHead from "@mui/material/TableHead";
 import TableRow from "@mui/material/TableRow";
 import Paper from "@mui/material/Paper";
+import { createTheme, ThemeProvider } from "@mui/material/styles";
+
+const theme = createTheme({
+  palette: {
+    primary: {
+      main: "#CC00CC", // เปลี่ยนสี primary ตามที่ต้องการ
+    },
+    background: {
+      default: "#66FFFF", // เปลี่ยนสีพื้นหลังหน้าเว็บตามที่ต้องการ
+    },
+  },
+});
 
 function App() {
   const [customers, setCustomers] = useState([]);
@@ -28,57 +40,59 @@ function App() {
   }, []);
 
   return (
-    <div>
-      <Box sx={{ flexGrow: 1 }}>
-        <AppBar position="static">
-          <Toolbar>
-            <IconButton
-              size="large"
-              edge="start"
-              color="inherit"
-              aria-label="menu"
-              sx={{ mr: 2 }}
-            >
-              <MenuIcon />
-            </IconButton>
-            <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
-              Rangsit University
-            </Typography>
-            <Button color="inherit">Login</Button>
-          </Toolbar>
-        </AppBar>
-      </Box>
-      <h1>RFID</h1>
-      <h2>ตารางบันทึกการเข้าใช้งาน</h2>
-      <TableContainer component={Paper}>
-        <Table sx={{ minWidth: 650 }} aria-label="simple table">
-          <TableHead>
-            <TableRow>
-              <TableCell>ID</TableCell>
-              <TableCell>IDCARD</TableCell>
-              <TableCell>IDStudent</TableCell>
-              <TableCell>ชื่อ</TableCell>
-              <TableCell>ห้อง</TableCell>
-              <TableCell>เวลา</TableCell>
-            </TableRow>
-          </TableHead>
-          <TableBody>
-            {customers.map((customer) => (
-              <TableRow key={customer.id}>
-                <TableCell component="th" scope="row">
-                  {customer.id}
-                </TableCell>
-                <TableCell>{customer.uid_tag}</TableCell>
-                <TableCell>{customer.id_student}</TableCell>
-                <TableCell>{customer.name}</TableCell>
-                <TableCell>{customer.action}</TableCell>
-                <TableCell>{customer.created_at}</TableCell>
+    <ThemeProvider theme={theme}>
+      <div>
+        <Box sx={{ flexGrow: 1 }}>
+          <AppBar position="static" color="primary">
+            <Toolbar>
+              <IconButton
+                size="large"
+                edge="start"
+                color="inherit"
+                aria-label="menu"
+                sx={{ mr: 2 }}
+              >
+                <MenuIcon />
+              </IconButton>
+              <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+                Rangsit University
+              </Typography>
+              <Button color="inherit">Login</Button>
+            </Toolbar>
+          </AppBar>
+        </Box>
+        <h1 style={{ marginTop: "20px" }}>RFID</h1>
+        <h2>ตารางบันทึกการเข้าใช้งาน</h2>
+        <TableContainer component={Paper}>
+          <Table sx={{ minWidth: 650 }} aria-label="simple table">
+            <TableHead>
+              <TableRow>
+                <TableCell>ID</TableCell>
+                <TableCell>IDCARD</TableCell>
+                <TableCell>IDStudent</TableCell>
+                <TableCell>ชื่อ</TableCell>
+                <TableCell>ห้อง</TableCell>
+                <TableCell>เวลา</TableCell>
               </TableRow>
-            ))}
-          </TableBody>
-        </Table>
-      </TableContainer>
-    </div>
+            </TableHead>
+            <TableBody>
+              {customers.map((customer) => (
+                <TableRow key={customer.id}>
+                  <TableCell component="th" scope="row">
+                    {customer.id}
+                  </TableCell>
+                  <TableCell>{customer.uid_tag}</TableCell>
+                  <TableCell>{customer.id_student}</TableCell>
+                  <TableCell>{customer.name}</TableCell>
+                  <TableCell>{customer.action}</TableCell>
+                  <TableCell>{customer.created_at}</TableCell>
+                </TableRow>
+              ))}
+            </TableBody>
+          </Table>
+        </TableContainer>
+      </div>
+    </ThemeProvider>
   );
 }
 
